5b834db7670142a036e9584fc80a55a3f793f5e6
Utsanjan Maity <utsanjan@gmail.com>
2023-04-26 21:10:37 +0530

    initial commit

---
+++ Tsunami/assets/js/client.js
@@ -0,0 +1,150 @@
+window['__wavt'] = 'AOuZoY40P_t6iAdN9ummbTO1vtFZFLlSzQ:1682496754618';
+_WidgetManager._Init('//www.blogger.com/rearrange?blogID\x3d7386992261808508022', '//web3hmkjpvnf3n1bewkovl2hws0tmdjhozgxr.blogspot.com/', '7386992261808508022');
+_WidgetManager._SetDataContext([{
+    'name': 'blog',
+    'data': {
+        'blogId': '7386992261808508022',
+        'title': 'BombADS',
+        'url': 'https://web3hmkjpvnf3n1bewkovl2hws0tmdjhozgxr.blogspot.com/',
+        'canonicalUrl': 'https://web3hmkjpvnf3n1bewkovl2hws0tmdjhozgxr.blogspot.com/',
+        'homepageUrl': 'https://web3hmkjpvnf3n1bewkovl2hws0tmdjhozgxr.blogspot.com/',
+        'searchUrl': 'https://web3hmkjpvnf3n1bewkovl2hws0tmdjhozgxr.blogspot.com/search',
+        'canonicalHomepageUrl': 'https://web3hmkjpvnf3n1bewkovl2hws0tmdjhozgxr.blogspot.com/',
+        'blogspotFaviconUrl': 'https://web3hmkjpvnf3n1bewkovl2hws0tmdjhozgxr.blogspot.com/favicon.ico',
+        'bloggerUrl': 'https://www.blogger.com',
+        'hasCustomDomain': false,
+        'httpsEnabled': true,
+        'enabledCommentProfileImages': true,
+        'gPlusViewType': 'FILTERED_POSTMOD',
+        'adultContent': false,
+        'analyticsAccountNumber': '',
+        'encoding': 'UTF-8',
+        'locale': 'en',
+        'localeUnderscoreDelimited': 'en',
+        'languageDirection': 'ltr',
+        'isPrivate': false,
+        'isMobile': false,
+        'isMobileRequest': true,
+        'mobileClass': '',
+        'isPrivateBlog': false,
+        'isDynamicViewsAvailable': true,
+        'feedLinks': '\x3clink rel\x3d\x22alternate\x22 type\x3d\x22application/atom+xml\x22 title\x3d\x22BombADS - Atom\x22 href\x3d\x22https://web3hmkjpvnf3n1bewkovl2hws0tmdjhozgxr.blogspot.com/feeds/posts/default\x22 /\x3e\n\x3clink rel\x3d\x22alternate\x22 type\x3d\x22application/rss+xml\x22 title\x3d\x22BombADS - RSS\x22 href\x3d\x22https://web3hmkjpvnf3n1bewkovl2hws0tmdjhozgxr.blogspot.com/feeds/posts/default?alt\x3drss\x22 /\x3e\n\x3clink rel\x3d\x22service.post\x22 type\x3d\x22application/atom+xml\x22 title\x3d\x22BombADS - Atom\x22 href\x3d\x22https://www.blogger.com/feeds/7386992261808508022/posts/default\x22 /\x3e\n',
+        'meTag': '\x3clink rel\x3d\x22me\x22 href\x3d\x22https://www.blogger.com/profile/00203219626028887678\x22 /\x3e\n',
+        'adsenseHostId': 'ca-host-pub-1556223355139109',
+        'adsenseHasAds': false,
+        'adsenseAutoAds': false,
+        'boqCommentIframeForm': true,
+        'loginRedirectParam': '',
+        'view': '',
+        'dynamicViewsCommentsSrc': '//www.blogblog.com/dynamicviews/4224c15c4e7c9321/js/comments.js',
+        'dynamicViewsScriptSrc': '//www.blogblog.com/dynamicviews/25ca246f95d4f5e2',
+        'plusOneApiSrc': 'https://apis.google.com/js/platform.js',
+        'disableGComments': true,
+        'sharing': {
+            'platforms': [{
+                'name': 'Get link',
+                'key': 'link',
+                'shareMessage': 'Get link',
+                'target': ''
+            }, {
+                'name': 'Facebook',
+                'key': 'facebook',
+                'shareMessage': 'Share to Facebook',
+                'target': 'facebook'
+            }, {
+                'name': 'BlogThis!',
+                'key': 'blogThis',
+                'shareMessage': 'BlogThis!',
+                'target': 'blog'
+            }, {
+                'name': 'Twitter',
+                'key': 'twitter',
+                'shareMessage': 'Share to Twitter',
+                'target': 'twitter'
+            }, {
+                'name': 'Pinterest',
+                'key': 'pinterest',
+                'shareMessage': 'Share to Pinterest',
+                'target': 'pinterest'
+            }, {
+                'name': 'Email',
+                'key': 'email',
+                'shareMessage': 'Email',
+                'target': 'email'
+            }],
+            'disableGooglePlus': true,
+            'googlePlusShareButtonWidth': 0,
+            'googlePlusBootstrap': '\x3cscript type\x3d\x22text/javascript\x22\x3ewindow.___gcfg \x3d {\x27lang\x27: \x27en\x27};\x3c/script\x3e'
+        },
+        'hasCustomJumpLinkMessage': false,
+        'jumpLinkMessage': 'Read more',
+        'pageType': 'index',
+        'pageName': '',
+        'pageTitle': 'BombADS'
+    }
+}, {
+    'name': 'features',
+    'data': {}
+}, {
+    'name': 'messages',
+    'data': {
+        'edit': 'Edit',
+        'linkCopiedToClipboard': 'Link copied to clipboard!',
+        'ok': 'Ok',
+        'postLink': 'Post Link'
+    }
+}, {
+    'name': 'template',
+    'data': {
+        'name': 'custom',
+        'localizedName': 'Custom',
+        'isResponsive': false,
+        'isAlternateRendering': false,
+        'isCustom': true
+    }
+}, {
+    'name': 'view',
+    'data': {
+        'classic': {
+            'name': 'classic',
+            'url': '?view\x3dclassic'
+        },
+        'flipcard': {
+            'name': 'flipcard',
+            'url': '?view\x3dflipcard'
+        },
+        'magazine': {
+            'name': 'magazine',
+            'url': '?view\x3dmagazine'
+        },
+        'mosaic': {
+            'name': 'mosaic',
+            'url': '?view\x3dmosaic'
+        },
+        'sidebar': {
+            'name': 'sidebar',
+            'url': '?view\x3dsidebar'
+        },
+        'snapshot': {
+            'name': 'snapshot',
+            'url': '?view\x3dsnapshot'
+        },
+        'timeslide': {
+            'name': 'timeslide',
+            'url': '?view\x3dtimeslide'
+        },
+        'isMobile': false,
+        'title': 'BombADS',
+        'description': '',
+        'url': 'https://web3hmkjpvnf3n1bewkovl2hws0tmdjhozgxr.blogspot.com/',
+        'type': 'feed',
+        'isSingleItem': false,
+        'isMultipleItems': true,
+        'isError': false,
+        'isPage': false,
+        'isPost': false,
+        'isHomepage': true,
+        'isArchive': false,
+        'isLabelSearch': false
+    }
+}]);
---
+++ Tsunami/assets/js/server.js
@@ -0,0 +1,156 @@
+function scrollBottom() {
+    window.scrollTo(99999, 99999);
+}
+if (document.addEventListener) document.addEventListener("DOMContentLoaded", scrollBottom, false)
+else if (window.attachEvent) window.attachEvent("onload", scrollBottom);
+
+window['__wavt'] = 'AOuZoY4KpCSioH7hDglqumIu4Yi4Kqp4FQ:1682232599892';
+_WidgetManager._Init('//www.blogger.com/rearrange?blogID\x3d513789961612317891', '//bombapi.blogspot.com/', '513789961612317891');
+_WidgetManager._SetDataContext([{
+    'name': 'blog',
+    'data': {
+        'blogId': '513789961612317891',
+        'title': 'BombAPI',
+        'url': 'https://bombapi.blogspot.com/',
+        'canonicalUrl': 'https://bombapi.blogspot.com/',
+        'homepageUrl': 'https://bombapi.blogspot.com/',
+        'searchUrl': 'https://bombapi.blogspot.com/search',
+        'canonicalHomepageUrl': 'https://bombapi.blogspot.com/',
+        'blogspotFaviconUrl': 'https://bombapi.blogspot.com/favicon.ico',
+        'bloggerUrl': 'https://www.blogger.com',
+        'hasCustomDomain': false,
+        'httpsEnabled': true,
+        'enabledCommentProfileImages': true,
+        'gPlusViewType': 'FILTERED_POSTMOD',
+        'adultContent': false,
+        'analyticsAccountNumber': '',
+        'encoding': 'UTF-8',
+        'locale': 'en',
+        'localeUnderscoreDelimited': 'en',
+        'languageDirection': 'ltr',
+        'isPrivate': false,
+        'isMobile': false,
+        'isMobileRequest': true,
+        'mobileClass': '',
+        'isPrivateBlog': false,
+        'isDynamicViewsAvailable': true,
+        'feedLinks': '\x3clink rel\x3d\x22alternate\x22 type\x3d\x22application/atom+xml\x22 title\x3d\x22BombAPI - Atom\x22 href\x3d\x22https://bombapi.blogspot.com/feeds/posts/default\x22 /\x3e\n\x3clink rel\x3d\x22alternate\x22 type\x3d\x22application/rss+xml\x22 title\x3d\x22BombAPI - RSS\x22 href\x3d\x22https://bombapi.blogspot.com/feeds/posts/default?alt\x3drss\x22 /\x3e\n\x3clink rel\x3d\x22service.post\x22 type\x3d\x22application/atom+xml\x22 title\x3d\x22BombAPI - Atom\x22 href\x3d\x22https://www.blogger.com/feeds/513789961612317891/posts/default\x22 /\x3e\n',
+        'meTag': '\x3clink rel\x3d\x22me\x22 href\x3d\x22https://www.blogger.com/profile/00203219626028887678\x22 /\x3e\n',
+        'adsenseHostId': 'ca-host-pub-1556223355139109',
+        'adsenseHasAds': false,
+        'adsenseAutoAds': false,
+        'boqCommentIframeForm': true,
+        'loginRedirectParam': '',
+        'view': '',
+        'dynamicViewsCommentsSrc': '//www.blogblog.com/dynamicviews/4224c15c4e7c9321/js/comments.js',
+        'dynamicViewsScriptSrc': '//www.blogblog.com/dynamicviews/25ca246f95d4f5e2',
+        'plusOneApiSrc': 'https://apis.google.com/js/platform.js',
+        'disableGComments': true,
+        'sharing': {
+            'platforms': [{
+                'name': 'Get link',
+                'key': 'link',
+                'shareMessage': 'Get link',
+                'target': ''
+            }, {
+                'name': 'Facebook',
+                'key': 'facebook',
+                'shareMessage': 'Share to Facebook',
+                'target': 'facebook'
+            }, {
+                'name': 'BlogThis!',
+                'key': 'blogThis',
+                'shareMessage': 'BlogThis!',
+                'target': 'blog'
+            }, {
+                'name': 'Twitter',
+                'key': 'twitter',
+                'shareMessage': 'Share to Twitter',
+                'target': 'twitter'
+            }, {
+                'name': 'Pinterest',
+                'key': 'pinterest',
+                'shareMessage': 'Share to Pinterest',
+                'target': 'pinterest'
+            }, {
+                'name': 'Email',
+                'key': 'email',
+                'shareMessage': 'Email',
+                'target': 'email'
+            }],
+            'disableGooglePlus': true,
+            'googlePlusShareButtonWidth': 0,
+            'googlePlusBootstrap': '\x3cscript type\x3d\x22text/javascript\x22\x3ewindow.___gcfg \x3d {\x27lang\x27: \x27en\x27};\x3c/script\x3e'
+        },
+        'hasCustomJumpLinkMessage': false,
+        'jumpLinkMessage': 'Read more',
+        'pageType': 'index',
+        'pageName': '',
+        'pageTitle': 'BombAPI'
+    }
+}, {
+    'name': 'features',
+    'data': {}
+}, {
+    'name': 'messages',
+    'data': {
+        'edit': 'Edit',
+        'linkCopiedToClipboard': 'Link copied to clipboard!',
+        'ok': 'Ok',
+        'postLink': 'Post Link'
+    }
+}, {
+    'name': 'template',
+    'data': {
+        'name': 'custom',
+        'localizedName': 'Custom',
+        'isResponsive': false,
+        'isAlternateRendering': false,
+        'isCustom': true
+    }
+}, {
+    'name': 'view',
+    'data': {
+        'classic': {
+            'name': 'classic',
+            'url': '?view\x3dclassic'
+        },
+        'flipcard': {
+            'name': 'flipcard',
+            'url': '?view\x3dflipcard'
+        },
+        'magazine': {
+            'name': 'magazine',
+            'url': '?view\x3dmagazine'
+        },
+        'mosaic': {
+            'name': 'mosaic',
+            'url': '?view\x3dmosaic'
+        },
+        'sidebar': {
+            'name': 'sidebar',
+            'url': '?view\x3dsidebar'
+        },
+        'snapshot': {
+            'name': 'snapshot',
+            'url': '?view\x3dsnapshot'
+        },
+        'timeslide': {
+            'name': 'timeslide',
+            'url': '?view\x3dtimeslide'
+        },
+        'isMobile': false,
+        'title': 'BombAPI',
+        'description': '',
+        'url': 'https://bombapi.blogspot.com/',
+        'type': 'feed',
+        'isSingleItem': false,
+        'isMultipleItems': true,
+        'isError': false,
+        'isPage': false,
+        'isPost': false,
+        'isHomepage': true,
+        'isArchive': false,
+        'isLabelSearch': false
+    }
+}]);
